c6a090003b085b392ee2c11dff018d25d6c47f24
nidadegirmenci <142792262+nidadegirmenci@users.noreply.github.com>
2024-11-21 14:37:31 +0300

    tsc settings

--- dist/common/router/register.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const register_controller_1 = require("../controller/register.controller"); // Doğru yolu buraya yazın
-const router = (0, express_1.Router)();
-const registerController = new register_controller_1.RegisterController();
-// Register endpoint
-router.post('/', registerController.register.bind(registerController));
-exports.default = router;
--- dist/question_management/entity/package.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuestionPackageRelation = void 0;
-const mongoose_1 = require("mongoose");
-const QuestionPackageRelationSchema = new mongoose_1.Schema({
-    questionText: { type: String, required: true },
-    packageName: { type: String, required: true },
-});
-exports.QuestionPackageRelation = (0, mongoose_1.model)('QuestionPackageRelation', QuestionPackageRelationSchema);
